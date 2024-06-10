777f0471ecb03506b5a4b9582e0d246ebb11f89b
Kaushall-raut <KaushalRaut.code@gmail.com>
2024-06-10 16:23:50 +0530

    last commit

---
+++ problem1.js
@@ -0,0 +1,12 @@
+function maxCustomers(X, Y) {
+  console.log(X * Y);
+}
+
+// Input related code. Please do not change this.
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", function (input) {
+  const nums = input.split(" ");
+  const x = parseInt(nums[0]);
+  const y = parseInt(nums[1]);
+  maxCustomers(x, y);
+});
